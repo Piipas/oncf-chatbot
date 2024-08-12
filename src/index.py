8aa87def76573e8a40454360c1db3c9c1a2cb55c
Piipas <ismailpipas@gmail.com>
2024-08-12 14:44:51 +0100

    feat: json file contains training data

--- src/index.py
+++ src/index.py
@@ -5,26 +5,27 @@ import tkinter as tk
 
 def get_response(user_input):
     response = chatbot.get_response(user_input)
-    if float(response.confidence) < 0.50:
+    print(response.confidence)
+    if float(response.confidence) < 0.40:
         response = Statement(
             text="Désolé, je ne comprends pas cette question.")
     return str(response)
 
 
 def send_message(event=None):
-    user_input = user_entry.get()
+    user_input = user_entry.get().lower()
     if user_input:
         chat_log.config(state=tk.NORMAL)
         chat_log.insert(tk.END, f"Vous: {user_input}\n")
         user_entry.delete(0, tk.END)
-        response = get_response(user_input)
+        response = get_response(user_input).replace('[newline]', '\n')
         chat_log.insert(tk.END, f"Bot: {response}\n")
         chat_log.config(state=tk.DISABLED)
         chat_log.yview(tk.END)
 
 
 root = tk.Tk()
-root.title("Intern Guide Bot")
+root.title("ONCF")
 
 chat_log = tk.Text(root, bd=1, bg="white", height=20,
                    width=50, font=("Arial", 12), state=tk.DISABLED)
--- src/train.py
+++ src/train.py
@@ -1,31 +1,14 @@
 from chatterbot.trainers import ListTrainer
 from init import chatbot
+import json
+import os
 
-dataset = [
-    ['Bonjour', 'Bonjour! Comment puis-je vous aider?'],
-    ["Je suis un nouvel stagiaire.",
-        "Bienvenue! De quoi avez-vous besoin pour commencer?"],
-    ["Pouvez-vous m'expliquer les tâches de la journée?",
-        "Bien sûr. Vous devez d'abord vérifier vos e-mails pour les nouvelles instructions."],
-    ["À qui puis-je demander de l'aide?",
-        "Vous pouvez demander de l'aide à votre superviseur ou à n'importe quel collègue."],
-    ["Quels outils vais-je utiliser?",
-        "Vous utiliserez principalement Google Workspace, Slack et GitHub."],
-    ["Merci", "De rien! Si vous avez d'autres questions, n'hésitez pas à demander."],
-    ["Quelles sont les horaires de travail?",
-        "Les horaires de travail sont de 9h à 17h, du lundi au vendredi."],
-    ["Où se trouve la cafétéria?",
-        "La cafétéria se trouve au rez-de-chaussée, près de l'entrée principale."],
-    ["Comment puis-je accéder au réseau Wi-Fi?",
-        "Vous pouvez accéder au réseau Wi-Fi en utilisant le SSID 'ONCF' et le mot de passe fourni par le service informatique."]
-]
-
+data_file = open(os.path.abspath('src/data.json'), 'r')
+dataset = json.load(data_file)['dataset']
 
 try:
     trainer = ListTrainer(chatbot)
     for qa in dataset:
-        print(qa)
         trainer.train(qa)
-
 except FileNotFoundError:
-    print('le fichier data.txt n\'existe pas! Essayer de creer un nouveau fichier qui contient la dataset!')
+    print('le fichier data.json n\'existe pas! Essayer de creer un nouveau fichier qui contient la dataset!')
